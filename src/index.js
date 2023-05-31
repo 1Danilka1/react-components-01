4abd6baed57535b5e58f0ab52c33e7d3e66afc9c
Danyl Brahan <danylbragan@gmail.com>
2023-05-31 16:16:34 +0200

    end of working

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+// import user from 'path/to/user.json';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const user = {
+  "username": "Jacques Gluke",
+  "tag": "jgluke",
+  "location": "Ocho Rios, Jamaica",
+  "avatar": "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
+  "stats": {
+    "followers": 5603,
+    "views": 4827,
+    "likes": 1308
+  }
+}
+
+
+function Painting(props) {
+  return (
+    <div class="profile">
+      <div class="description">
+        <img src={user.avatar} alt={user.tag} class="avatar"></img>
+        <p class="name">{user.username}</p>
+        <p class="tag">@{user.tag}</p>
+        <p class="location">{user.location}</p>
+      </div>
+      <ul class="stats">
+        <li>
+          <span class="label">Followers</span>
+          <span class="quantity"> - {user.stats.followers}</span>
+        </li>
+        <li>
+          <span class="label">Views</span>
+          <span class="quantity"> - {user.stats.views}</span>
+        </li>
+        <li>
+          <span class="label">Likes</span>
+          <span class="quantity"> - {user.stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+ReactDOM.render(<Painting />, document.querySelector("#root"));
